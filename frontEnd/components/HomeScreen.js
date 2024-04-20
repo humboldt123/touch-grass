7b05e3d5185ebd2c2548ddc1c1b30ff679d7d203
vish <38266782+humboldt123@users.noreply.github.com>
2024-04-20 18:40:18 -0400

    make it look prettier

--- frontEnd/components/HomeScreen.js
+++ frontEnd/components/HomeScreen.js
@@ -1,5 +1,7 @@
 import React, { useEffect } from 'react';
-import { View, Text, Button, Image, StyleSheet} from 'react-native';
+import { View, Text, Pressable, Image, StyleSheet} from 'react-native';
+import { useFonts, Lora_400Regular, Lora_700Bold } from "@expo-google-fonts/lora";
+
 // import { fetchData } from './api/apiClient';
 
 
@@ -24,6 +26,15 @@ const HomeScreen = () => {
     }
   };
 
+  
+  let [fontsLoaded, fontError] = useFonts({
+    Lora_400Regular, Lora_700Bold
+  });
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   // fetch Data from localhost:3000
   const fetchData = async () => {
     try {
@@ -46,11 +57,15 @@ const HomeScreen = () => {
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#dedede'}}>
       <View style={styles.card}>
         <View style={styles.cardBackground}>
-          <Image source={{uri: "https://schuylkillyards.com/sites/default/files/styles/max_1300x1300/public/gallery2020-07/0166-E057.jpg"}} style={styles.banner} resizeMode="cover" blurRadius={3}/>
-          <Text>Event Name</Text>
-          <Text>129 S 30th St @ 12:00 PM </Text>
-          <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nec dui nunc mattis enim ut. Enim ut sem viverra aliquet eget sit amet tellus. Non odio euismod lacinia at quis. Dictum non consectetur a erat nam at.</Text>
-          <Button title='Attend'></Button>
+          <Image source={{uri: "https://schuylkillyards.com/sites/default/files/styles/max_1300x1300/public/gallery2020-07/0166-E057.jpg"}} style={[styles.banner, {top: -100}]} resizeMode="cover" blurRadius={3}/>
+          <View style={{top: -50}}>
+            <Text style={[{textAlign: "center", fontSize: 30}, styles.lora]}>Event Name</Text>
+            <Text style={[{textAlign: "center", fontSize: 14, marginTop: 8}, styles.lora]}>129 S 30th St @ 12:00 PM </Text>
+            <Text style={[{marginTop: 40, marginLeft: 20, marginRight: 20}, styles.lora]}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nec dui nunc mattis enim ut. Enim ut sem viverra aliquet eget sit amet tellus. Non odio euismod lacinia at quis. Dictum non consectetur a erat nam at.</Text>
+          </View>
+          <Pressable style={styles.button}>
+            <Text style={[styles.loraBold, {color: 'white'}]}>Attend</Text>
+          </Pressable>
         </View>
       </View>
 
@@ -63,7 +78,7 @@ const HomeScreen = () => {
           />
         ))}
         <Text>
-          Basil, Mari, and 2 others are attending
+          Basil, Mari and two others are attending
         </Text>
       </View>
     </View>
@@ -71,6 +86,26 @@ const HomeScreen = () => {
 };
 
 const styles = StyleSheet.create({
+  lora: {
+    fontFamily: "Lora_400Regular"
+  },
+  loraBold: {
+    fontFamily: "Lora_700Bold"
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    marginLeft: 40,
+    marginRight: 40,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: '#E85380',
+    shadowColor: '#000',
+    shadowOpacity: 0.2,
+    shadowRadius: 2,
+    shadowOffset: {width:2,height:4},
+  },
   card: {
     shadowColor: '#000',
     shadowOpacity: 0.2,
@@ -89,7 +124,6 @@ const styles = StyleSheet.create({
   banner: {
     width: 307,
     height: 207,
-    top: -100,
   },
   participants: {
     flexDirection: 'row',
